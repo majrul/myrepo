b69b9ff2fada6e62b52b1c8a82bbe5d8a5c6892c
majrul <majrul@192.168.8.101>
2018-10-12 10:58:22 +0530

    Fifth Commit

--- spring-boot-trial/src/main/java/com/training/demo/SpringBootTrialApplication.java
+++ spring-boot-trial/src/main/java/com/training/demo/SpringBootTrialApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootTrialApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Loading Spring Boot..");
 		SpringApplication.run(SpringBootTrialApplication.class, args);
 	}
 }
